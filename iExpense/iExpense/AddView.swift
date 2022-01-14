43f554e78090e7dace47e6a3022a0e38ee4c02a2
j.TRUbela <81325704+jtrubela@users.noreply.github.com>
2022-01-14 14:00:40 -0500

    Update AddView.swift

--- iExpense/iExpense/AddView.swift
+++ iExpense/iExpense/AddView.swift
@@ -9,10 +9,16 @@ import SwiftUI
 
 struct AddView: View {
     
+    // @ObservedObject - when modifying the object - when you are just reading and writing
     @ObservedObject var expenses: Expenses
-    @ObservedObject var businessExpenses: Expenses
+    @ObservedObject var businessExpenses: BusinessExpenses
+    
+    // Sharing/Showing views in SwiftUI - sheet - a new view presented on top of our existing one. (credit card-like presentation)
+    // define the conditions in which the sheet should be shown
+    // and when those conditions become true of false the sheet will either be presented or dismissed respectively
     @Environment(\.dismiss) var dismiss
     
+    // @State is for sharing across one local view
     @State private var name = ""
     @State private var type = "Personal"
     @State private var amount = 0.0
@@ -44,7 +50,7 @@ struct AddView: View {
                     }
                     else {
                         let item = ExpenseItem(name: name, type: type, amount: amount)
-                        businessExpenses.items.append(item)
+                        businessExpenses.businessItems.append(item)
                     }
                     
                     dismiss()
@@ -56,6 +62,6 @@ struct AddView: View {
 
 struct AddView_Previews: PreviewProvider {
     static var previews: some View {
-        AddView(expenses: Expenses(), businessExpenses: Expenses())
+        AddView(expenses: Expenses(), businessExpenses: BusinessExpenses())
     }
 }
