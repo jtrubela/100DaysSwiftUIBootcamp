97cef9fd9e9ba8a29715d6c7c718e2de88c20859
j.TRUbela <81325704+jtrubela@users.noreply.github.com>
2024-03-06 04:45:57 -0500

    Add files via upload

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Justin Trubela on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
