758cee9400d3d7dcc2e5577f5bdaaea98d6e459d
j.TRUbela <81325704+jtrubela@users.noreply.github.com>
2024-03-06 04:46:52 -0500

    Add files via upload

---
+++ Cupcake Corner/Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Justin Trubela on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
